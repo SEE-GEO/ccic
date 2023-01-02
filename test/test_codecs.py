1d9962b7ade9ecb6e76a8a394373e9f76320ead5
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2023-01-02 21:04:51 +0100

    Added another test for custom codecs.

--- test/test_codecs.py
+++ test/test_codecs.py
@@ -2,6 +2,8 @@
 Test custom codec for compressing IWP data.
 """
 import numpy as np
+import xarray as xr
+import zarr
 
 from ccic.codecs import LogBins
 
@@ -11,8 +13,8 @@ def test_codec():
     Ensures that encoding error are small and that NAN values are
     handled correctly.
     """
-    codec = LogBins(1e-3, 1e1)
-    values = np.logspace(-3, 1, 10_000)
+    codec = LogBins(1e-3, 1e2)
+    values = np.logspace(-3, 2, 10_000).astype(np.float32)
     values[0] = 1e-4
     values[-1] = np.nan
     values_enc = codec.encode(values)
@@ -26,7 +28,36 @@ def test_codec():
 
     assert np.isclose(values_dec[0], 0.0)
     # Discretization error is within +/- 2% for chosen range.
-    assert np.all(np.isclose(values_dec[1:-1], values[1:-1], rtol=2e-2))
+    assert np.all(np.isclose(values_dec[1:-1], values[1:-1], rtol=3e-2))
     assert np.isnan(values_dec[-1])
     assert np.all(np.isfinite(values_dec[:-1]))
+
+
+def test_save_dataset(tmpdir):
+    """
+    Tests saving an xarray.Dataset using the LogBins codec.
+    """
+    data = 10 ** (np.random.uniform(-3, 2, size=(1_000, 1_000))).astype(np.float32)
+    dataset = xr.Dataset({
+        "iwp": (("y", "x"), data)
+    })
+
+    filters = [LogBins(1e-3, 1e2)]
+    compressor = zarr.Blosc(cname="zstd", clevel=3, shuffle=2)
+
+    encodings = {
+        "iwp": {
+            "compressor": compressor,
+            "filters": filters
+        }
+    }
+    dataset.to_zarr(tmpdir / "test.zarr", encoding=encodings, compute=True)
+
+    dataset_dec = xr.open_zarr(tmpdir / "test.zarr")
+
+    iwp = dataset.iwp.data
+    iwp_dec = dataset_dec.iwp.data
+
+    # Discretization error is within +/- 2% for chosen range.
+    assert np.all(np.isclose(iwp, iwp_dec, rtol=3e-2))
 
