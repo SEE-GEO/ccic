cbab3f5fcabcf0aa396285231eff1ca2b3b97582
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2023-02-13 13:55:54 +0100

    Add download functions for radar data and ERA5.

--- ccic/validation/input_data.py
+++ ccic/validation/input_data.py
@@ -252,7 +252,7 @@ class RetrievalInput(Fascod):
                 kwargs={"fill_value": "extrapolate"}
             )
             self._data = xr.merge([era5_data, radar_data])
-            self.interpolate_altitude(self._data.altitude.data)
+            self.interpolate_altitude(self._data.height.data)
 
     def get_radar_reflectivity(self, date):
         """
@@ -307,14 +307,16 @@ class RetrievalInput(Fascod):
     def get_altitude(self, date):
         """Get the altitude in the atmospheric column above the radar."""
         self._load_data(date)
-        return self._data.altitude.data
+        return self._data.height.data
 
     def get_surface_altitude(self, date):
         """Get the surface altitude."""
-        return np.array([[self.radar.elevation]])
+        self._load_data(date)
+        return self._data.height.data[0]
 
     def get_radar_sensor_position(self, date):
-        return np.array([[self.radar.elevation]])
+        self._load_data(date)
+        return np.array([[self._data.height.data[0]]])
 
     def get_h2o(self, date):
         """Get H2O VMR in the atmospheric column above the radar."""
